c66df6baa7721dc526d99cff3ecc317c80111d22
Szász Tibor <tszasz@gmail.com>
2017-05-11 14:50:52 +0200

    feat(api): basic communication works

--- js/ajax.js
+++ js/ajax.js
@@ -10,6 +10,7 @@ function ajax(url, callback) {
 		if(xhr.readyState === XMLHttpRequest.DONE) {
 			if(xhr.status === 200) {
 				var rsp = JSON.parse( xhr.response );
+				callback(rsp);
 			} else {
 				new Error('API PANIC!!!');
 				console.log(xhr.status);
--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
 
+var button = document.querySelector('#action');
+var keyword = document.querySelector('#keyword');
+var wResults = document.querySelector('#wordnik-results');
+var imageWrapper = document.querySelector('#image-wrapper');
+
+function getWordnikData(keyword) {
+	const endpoint = 'http://api.wordnik.com:80/v4/word.json/'+ keyword +'/relatedWords?useCanonical=false&limitPerRelationshipType=10&api_key=' + wordnik_key;
+	ajax(endpoint, function (response) {
+		displayWordnikResults(response);
+	});
+}
+
+function displayWordnikResults(relatedData) {
+	relatedData.forEach(function(relationship){
+		var header = document.createElement('h3');
+		header.innerText = relationship.relationshipType;
+		wResults.appendChild(header);
+
+		relationship.words.forEach(function(word){
+			var anchor = document.createElement('a');
+			anchor.innerText = word;
+			anchor.href = ''
+			anchor.addEventListener('click', function (e) {
+				e.preventDefault()
+				getGoogleData( word );
+			});
+			wResults.appendChild(anchor);
+		});
+	});
+}
+
+function getGoogleData(keyword) {
+	const endpoint = 'https://www.googleapis.com/customsearch/v1?q='+ keyword +'&cx=' + google_se + '&searchType=image&key=' + google_api_key
+	ajax(endpoint, function (response) {
+		displayGoogleResults(response);
+	});
+}
+
+function displayGoogleResults(gData) {
+	gData.items.forEach(function (item) {
+		var img = document.createElement('img');
+		img.src = item.image.thumbnailLink;
+		imageWrapper.appendChild(img);
+	});
+}
+
+
+button.addEventListener('click', function () {
+	getWordnikData(keyword.value)
+});
